90863ccff048ff90964843f0408d360d1c3766a2
joydan <joydan@Joyananths-MacBook-Pro.local>
2020-07-16 22:47:38 -0400

    This is first comit

---
+++ boards/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User
+from .forms import NewTopicForm
+
+# Create your views here.
+from django.http import HttpResponse
+from django.http import Http404
+
+def home(request):
+	'''boards = Board.objects.all()
+				boards_name = list()
+				for board in boards:
+					boards_name.append(board.name)
+			
+				response_html = '<br>'.join(boards_name)
+				return HttpResponse(response_html)'''
+	boards = Board.objects.all()
+
+	return render(request,'home.html',{'boards':boards})
+
+def board_topics(request,pk):
+	try:
+		#board = Board.objects.get(pk=pk)
+		board = get_object_or_404(Board, pk=pk)
+	except Board.DoesNotExist:
+		raise Http404
+	return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
+    if request.method == "POST":
+    	form = NewTopicForm(request.POST)
+    	if form.is_valid():
+    		topic = form.save(commit=False)
+    		topic.board = board
+    		topic.starter = user
+    		topic.save()
+    		post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+    	return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
